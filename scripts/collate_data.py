176697c1eecd3a02fad8d936d2185477dac6236f
Austin Zadoks <austin.zadoks@protonmail.com>
2024-07-08 23:05:45 +0200

    Tweak dependencies, loosen pre-commit python refactor collate data to store many features in one file (bad idea?)

--- scripts/collate_data.py
+++ scripts/collate_data.py
@@ -6,22 +6,34 @@ import numpy as np
 
 
 # %%
-def load_data(features: str = "dos_fermi_centered", smearing_type: str = "gauss", smearing_width: float = 0.05):
+def load_data(
+    calculation: str = "zero_shot",
+    features: list[str] = ["fermi_centered", "vbm_centered", "fermi_scissor"],
+    smearing_type: str = "gauss",
+    smearing_width: float = 0.05,
+):
     data = {
         "sorep_id": [],
-        "features": [],
         "meets_tcm_criteria": [],
         "band_gap": [],
         "electron_effective_mass": [],
         "hole_effective_mass": [],
+        **{feature: [] for feature in features}
     }
     dirs = list(pl.Path("../data/mc3d/").glob("*"))
     for dir_ in dirs:
-        if (dir_ / "zero_shot").exists() and (dir_ / "bands").exists():
+        if (dir_ / calculation).exists() and (dir_ / "bands").exists():
             with open(dir_ / "bands" / "tcm_criteria.json", "r", encoding="utf-8") as fp:
                 criteria = json.load(fp)
-            with open(dir_ / "zero_shot" / f"dos_{smearing_type}_{smearing_width}.npz", "rb") as fp:
-                data["features"].append(np.load(fp)[features].sum(axis=0))  # Sum over spin channels
+            with open(dir_ / calculation / f"dos_{smearing_type}_{smearing_width}.npz", "rb") as fp:
+                npz = np.load(fp)
+                for feature in features:
+                    feature_array = npz[f'dos_{feature}']
+                    if feature_array.ndim == 2:
+                        feature_array = feature_array.sum(axis=0)  # Sum over spin channels
+                    assert feature_array.ndim == 1
+                    data[feature].append(feature_array)
+
             data["meets_tcm_criteria"].append(
                 criteria["meets_band_gap"]
                 and criteria["meets_electron_effective_mass"]
@@ -39,9 +51,11 @@ def load_data(features: str = "dos_fermi_centered", smearing_type: str = "gauss"
 
 
 # %%
-for features in ("dos_fermi_centered", "dos_vbm_centered", "dos_fermi_scissor"):
-    smearing_type = "gauss"
-    smearing_width = 0.05
-    np.savez_compressed(f"../data/mc3d_{features}_{smearing_type}_{smearing_width}.npz", **load_data())
+CALCULATION = "zero_shot"
+FEATURES = ["fermi_centered", "vbm_centered", "fermi_scissor"]
+SMEARING_TYPE = "gauss"
+SMEARING_WIDTH = 0.05
+data = load_data(CALCULATION, FEATURES, SMEARING_TYPE, SMEARING_WIDTH)
+np.savez_compressed(f"../data/mc3d_{CALCULATION}_{SMEARING_TYPE}_{SMEARING_WIDTH}.npz", **load_data())
 
 # %%
